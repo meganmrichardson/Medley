872d3133d7bfd5f30d1092e82f4e72155a4d0ad7
tigerlillyz <tigerlillyzietz@gmail.com>
2021-03-11 08:03:49 -1000

    ast builder not working

--- src/medley.js
+++ src/medley.js
@@ -1,1 +1,32 @@
-console.log("Medley Compiler");
+#! /usr/bin/env node
+
+const fs = require("fs/promises")
+const process = require("process")
+const compile = require("./compiler.js")
+
+const help = `Medley compiler
+Syntax: src/medley.js <filename> <outputType>
+Prints to stdout according to <outputType>, which must be one of:
+  ast        the abstract syntax tree
+  analyzed   the semantically analyzed representation
+  optimized  the optimized semantically analyzed representation
+  js         the translation to JavaScript
+  c          the translation to C
+  llvm       the translation to LLVM
+`
+
+async function compileFromFile(filename, outputType) {
+  try {
+    const buffer = await fs.readFile(filename)
+    console.log(compile(buffer.toString(), outputType))
+  } catch (e) {
+    console.error(`${e}`)
+    process.exitCode = 1
+  }
+}
+
+if (process.argv.length !== 4) {
+  console.log(help)
+} else {
+  compileFromFile(process.argv[2], process.argv[3])
+}
